b388cf4e2b05f46929f114546dc5f11ce91f67fb
sumayaakter533 <sumaya55230@gmail.com>
2024-10-01 09:14:53 +0600

    js-common-concept-7 | closure.js added

---
+++ common-js-concepts/#7_closure.js
@@ -0,0 +1,30 @@
+function kitchen() {
+    let roast = 0;
+    return function(){
+        roast++;
+        return roast;
+    }
+}
+
+const firstServer = kitchen();
+console.log(firstServer);
+console.log(firstServer());
+console.log(firstServer());
+console.log(firstServer());
+console.log(firstServer());
+console.log(firstServer());
+console.log(firstServer());
+console.log(firstServer());
+console.log(firstServer());
+
+const secondServer = kitchen();
+console.log(secondServer);
+console.log(secondServer());
+console.log(secondServer());
+console.log(secondServer());
+
+console.log(firstServer());
+console.log(firstServer());
+console.log(firstServer());
+
+// NOTE when you declare a function inside another function, then declared function can access all the properties(variables) of the parent function.
