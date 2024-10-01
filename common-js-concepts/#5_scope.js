9d0cd591df69d8801230d7f7ef6523d584de8ca5
sumayaakter533 <sumaya55230@gmail.com>
2024-10-01 08:47:52 +0600

    js-common-concept-5 | scope.js modified

--- common-js-concepts/#5_scope.js
+++ common-js-concepts/#5_scope.js
@@ -1,17 +1,23 @@
+// ANCHOR function scope
+// everything between a function's curly braces is knowns as functional scope elements.
 function add(a, b) {
     const total = a + b;
     // console.log(a, b);
 
-    if(b > 5){
+    // ANCHOR block scope
+    // everything between different js element's curly braces is knowns as block scope elements.
+    if(b < 5){
+        // if block element
         const sum = 25 + a + b; 
     }else {
         const sum = 5 + a + b;
         var current = sum;
+        // NOTE var doesn't follow the block scope characteristics. when we name a variable with var inside a block scope, it doesn't follow the block scope and take the variable at the top level. it is called hoisting. that's we should avoid using var to name a variable.
     }
 
-    // console.log(sum);
+    // console.log(sum); //! ReferenceError: sum is not defined
     console.log(current);
-    return total;
+    return current;
 }
 
 // console.log(a, b); //! ReferenceError: a is not defined
